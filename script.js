5a733855544f2ee262cfdfe8730d10a614d1c29a
haamid10 <apdixamiid919@gmail.com>
2022-10-01 20:21:55 +0300

    main

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function moveRight(){
   var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
   character.style.left = left +2 + "px";
 }
+
+document.addEventListener("keydown", event => {
+  if(event.key==="Arrowleft"){
+    interval =setinterval(moveleft,1)
+  }
+  if(event.key==="ArrowRight"){
+    interval =setinterval(moveRight,1)
+  }
+
+})
