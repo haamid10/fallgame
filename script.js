811bbee70a3bbed0af05682ffb556d4b2541925e
haamid10 <apdixamiid919@gmail.com>
2022-10-01 20:37:15 +0300

    main

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 var character = document.getElementById("character")
+var interval;
+var both = 0;
 
 function moveLeft(){
   var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-  character.style.left = left +2 + "px";
+  character.style.left = left + 2 + "px";
 }
 
 function moveRight(){
   var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-  character.style.left = left +2 + "px";
+  character.style.left = left - 2 + "px";
 }
 
 document.addEventListener("keydown", event => {
-  if(event.key==="Arrowleft"){
-    interval =setinterval(moveleft,1)
+  if(both ===0){
+    both++;
+  if(event.key==="ArrowLeft"){
+    interval = setInterval(moveLeft,1)
   }
   if(event.key==="ArrowRight"){
-    interval =setinterval(moveRight,1)
+    interval =setInterval(moveRight,1)
   }
+  }
+})
 
+document.addEventListener("keyup" , event =>{
+  clearInterval(interval);
+  both = 0;
 })
